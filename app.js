1c500ced71b749e56bb80ce28f1c155341e9dc4f
Thomas Peterson <tap6@email.sc.edu>
2024-10-18 12:18:32 -0400

    Added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require("http");
+const port = process.env.PORT || 3001;
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(port);
+
+  //node app.js to launch on local
+  //ctrl + c to close it down.
+  
