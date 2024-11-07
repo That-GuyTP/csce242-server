091592bc17a21072acdbee6bec431d11beee130d
Thomas Peterson <tap6@email.sc.edu>
2024-11-07 10:01:58 -0500

    "adding stuffs"

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const port = process.env.PORT || 3001;
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World! Hello Thomas!");
+    res.end("Hello World! Hello Thomas! Test 1 Test 2 Test 3 Test 4 Test 5 Test461");
   })
   .listen(port);
-
-  //node app.js to launch on local
-  //ctrl + c to close it down.
